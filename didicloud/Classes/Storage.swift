60a822361ec994cf73eb8925805450d7dd7f49d7
Eduardo Renani <eduardo.renani@hotmail.com>
2020-07-14 11:46:56 -0300

    retrieving user ID and fetching records by it

--- didicloud/Classes/Storage.swift
+++ didicloud/Classes/Storage.swift
@@ -17,6 +17,63 @@ public struct Storage {
         return CKRecord.ID(recordName: name)
     }
     
+    /// Returns the current user icloud ID
+    /// - Parameter completion: Result object containing the user icloud ID or an error
+    public static func getUserRecordID(_ completion: @escaping (Result<CKRecord.ID, Error>) -> Void) {
+        CKContainer.default().fetchUserRecordID { (result, error) in
+            
+            if error != nil {
+                completion(.failure(StorageError.cloudKitDataRetrieval))
+                return
+            }
+            
+            guard let result = result else {
+                completion(.failure(StorageError.cloudKitNullReturn))
+                return
+            }
+            
+            completion(.success(result))
+            
+        }
+    }
+    
+    
+    /// Fetch all records of T owned by current user
+    /// - Parameters:
+    ///   - storageType: Which database to perform the query
+    ///   - completion: Result object containing all fetched records or an error
+    public static func fetchRecordsByUser<T: Storable>(storageType: StorageType = .privateStorage, _ completion: @escaping (Result<[T], Error>) -> Void) {
+        
+        getUserRecordID { (result) in
+            switch result {
+            case .success(let recordID):
+                let query = CKQuery(
+                    recordType: T.reference,
+                    predicate: NSPredicate(format: "creatorUserRecordID == %@", recordID)
+                )
+                
+                storageType.database.perform(query, inZoneWith: nil) {
+                    results, error in
+                    
+                    if error != nil {
+                        completion(.failure(StorageError.cloudKitDataRetrieval))
+                        return
+                    }
+                    
+                    guard let results = results else {
+                        completion(.failure(StorageError.cloudKitNullReturn))
+                        return
+                    }
+                    
+                    let values = results.map({ T.init($0) })
+                    completion(.success(values))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
     public static func getAll<T: Storable>(storageType: StorageType = .privateStorage, _ completion: @escaping (Result<[T], Error>) -> Void) {
         
         let query = CKQuery(recordType: T.reference, predicate: NSPredicate(value: true))
