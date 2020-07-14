87ea1a62b76023859515c8b53eccd63a67e6269c
Rodrigo <rodrigofgiglio@gmail.com>
2020-07-14 15:55:35 -0300

    adds custom init for CKObjects

--- didicloud/Classes/Storable.swift
+++ didicloud/Classes/Storable.swift
@@ -14,7 +14,28 @@ public protocol Referenciable {
     static var reference: String { get }
 }
 
-public typealias CKObject = CKDataObject
+open class CKObject: CKDataObject {
+    
+    public required convenience init(_ record: CKRecord) {
+        let dict = record.toDictionary()
+        self.init(dictionary: dict)
+        self.recordID = record.recordID
+        self.recordType = record.recordType
+        self.creationDate = record.creationDate ?? Date()
+        self.creatorUserRecordID = record.creatorUserRecordID
+        self.modificationDate = record.modificationDate ?? Date()
+        self.lastModifiedUserRecordID = record.lastModifiedUserRecordID
+        self.recordChangeTag = record.recordChangeTag
+        
+        let data = NSMutableData()
+        let coder = NSKeyedArchiver(forWritingWith: data)
+        record.encodeSystemFields(with: coder)
+        coder.finishEncoding()
+        self.encodedSystemFields = data as Data
+    }
+    
+}
+
 
 public protocol Storable: Referenciable & CKObject {
     
